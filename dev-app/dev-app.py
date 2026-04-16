275de2167defb796b6e8b26498c8cd3155104501
Henry Yang <henryyang98@yahoo.com>
2026-04-16 16:17:11 -0700

    Changed dev-app names and headers for each app

--- dev-app/dev-app.py
+++ dev-app/dev-app.py
@@ -4,13 +4,13 @@ app = Flask(__name__)
 def dev_home():
     return render_template('/dev-app.html')
 
-@app.route('/dev-app/repos')
-def repos():
-    return render_template('/dev-app-repos.html')
+@app.route('/dev-app/proj')
+def proj():
+    return render_template('/dev-app-proj.html')
 
-@app.route('/dev-app/builds')
-def builds():
-    return render_template('/dev-app-builds.html')
+@app.route('/dev-app/buildStatuses')
+def build_statuses():
+    return render_template('/dev-app-buildStatuses.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
